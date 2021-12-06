8e296de2cd66466c3f97f1c8978982fee2cb2677
eapenzacharias <eapenzac@gmail.com>
2021-12-06 17:08:10 +0100

    React setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>React app setup</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
