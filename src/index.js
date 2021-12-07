a65930ebae17fdc157531dd2bd51813ea8dc2ae7
eapenzacharias <eapenzac@gmail.com>
2021-12-07 20:10:11 +0100

    Updated fwith hashrouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
 import './styles/style.scss';
 
