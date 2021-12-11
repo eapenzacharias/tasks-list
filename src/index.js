8ce13d33be9c9191e25613b543ef89e9d4da9fe1
Eapen Zacharias <eapenzac@gmail.com>
2021-12-11 20:59:09 +0100

    Revert "Todo components"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
-import TodoContainer from './components/TodoContainer';
 import './styles/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <TodoContainer />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const element = <h1>React app setup</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
